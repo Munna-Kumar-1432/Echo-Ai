a9c309b5d06742a432246cba237457285dc79e11
Munna Kumar <munnakumar6204835946@gmail.com>
2025-08-27 22:06:25 +0530

    trpc-server and trpc-client

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,14 +1,9 @@
 "use client";
-
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC()
-  const {data} = useQuery(trpc.hello.queryOptions({text:"Munna Kumar"}))
+ 
   return (
     <div>
-       {data?.greeting}
+      Munna
     </div>
   );
 };
